02b1840776e76a2f84ca128ca4edd93b0767b1d0
Luis_Miguel <91768652+CallMeDad72@users.noreply.github.com>
2022-12-20 09:23:05 +0100

    Add files via upload

---
+++ CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace System // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Dime tu nombre");
+            name = Console.ReadLine();
+            Console.WriteLine("Hola " + name);
+        }
+    }
+}
+
+
---
+++ Ejercicio4/2+2 son cuatro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //He modificado los valores de x, y, z
+            int x = 7;
+            int y = 3;
+            int z = 2;
+    
+            int add = (x + y)/z;
+            Console.WriteLine("El resultado es " + add);
+
+        }
+    }
+}
